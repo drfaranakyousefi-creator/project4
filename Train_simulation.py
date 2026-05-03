8cd2bba6fd9f64d66b3116d85aa377992aceb203
drfaranakyousefi-creator <drfaranakyousefi@gmail.com>
2026-05-03 16:21:05 +0330

    Update Train_simulation.py

--- Train_simulation.py
+++ Train_simulation.py
@@ -84,18 +84,18 @@ class CAT(nn.Module) :
 
         return history
 
-
     def train_one_epoch(self):
-        for x, l, mask in self.data.train_loader:
+        for i, (x, l, mask) in enumerate(self.data.train_loader):
             v, loss_client = self.network(x.to(self.device), mask)
             grad = self.transmittion.send_data(v, l, status='train')
             
-            print("v mean:", v.mean().item(), "v std:", v.std().item())
-            print("grad mean:", grad.abs().mean().item(), "grad max:", grad.abs().max().item())
-            print("loss_client:", loss_client.item())
+            # فقط اولین batch رو پرینت میکنه
+            if i == 0:
+                print("v mean:", v.mean().item(), "v std:", v.std().item())
+                print("grad mean:", grad.abs().mean().item(), "grad max:", grad.abs().max().item())
+                print("loss_client:", loss_client.item())
             
             self.network.train_one_batch(loss_client, v, grad.clone())
-            break  # فقط یه batch چک میکنیم
         return True
 
     def evaluate_one_epoch(self)  :
