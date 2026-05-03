d223f47a3f9f0525c1831dd4538b556f2a552f2d
drfaranakyousefi-creator <drfaranakyousefi@gmail.com>
2026-05-03 17:04:48 +0330

    Update client_net.py

--- client_net.py
+++ client_net.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import math
-سس
+
 class LayerNormalization(nn.Module):
 
     def __init__(self, features: int, eps:float=10**-6) -> None:
